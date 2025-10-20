24aa354c07ebdcc6b2d1f3822a9c556a6feb3381
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-20 17:08:02 -0500

    adding ex0

---
+++ ex0.js
@@ -0,0 +1,13 @@
+const numbers = [1, 1, 2, 3, 5, 8, 13, 21];
+
+// print whether there are any odd numbers
+// (true or false)
+for (const num of numbers) {
+  // your code here
+}
+
+// print all the odd numbers
+// use a continue!
+for (const num of numbers) {
+  // your code here
+}
