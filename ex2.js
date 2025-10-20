4992211ca35825b617ce6e669d0963f06e56d3e1
Yulong Wu <wu.yulong@wustl.edu>
2025-10-20 17:45:24 -0500

    ex2

--- ex2.js
+++ ex2.js
@@ -7,5 +7,16 @@ const studentCountByStanding = {
 };
 
 // your code here
+let maxStanding = null;
+let maxCount = 0;
+for (const standing in studentCountByStanding) {
+  const count = studentCountByStanding[standing];
+  if (count > maxCount) {
+    maxCount = count;
+    maxStanding = standing;
+  }
+}
+
+console.log(`${maxStanding}: ${maxCount}`);
 
 // print which kind of student there are the most of, and how many there are.
